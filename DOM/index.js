a11fdd8fa50c5a9a39d5407fa8380ba6f7abae9a
ayush guleria <ayushguleriacu@gmail.com>
2026-01-27 13:26:25 +0530

    Added

---
+++ DOM/index.js
@@ -0,0 +1,13 @@
+// console.log("This is the DOM/index.js file.");
+// let heading=document.getElementById("heading");
+// console.log(heading);
+// let para=document.getElementsByClassName("paragraph");
+// console.log(para);
+// let tags=document.getElementsByTagName("p");
+// console.log(tags);
+let heading=document.querySelector("#heading")
+console.log(heading);
+let para=document.querySelectorAll(".paragraph")
+console.log(para);
+let tags=document.querySelectorAll("p") 
+console.log(tags);
---
+++ tem.js
@@ -0,0 +1,10 @@
+let a =10
+let nam ="ayush"
+let val=true
+let value
+let values= null
+console.log(typeof a)
+console.log(typeof nam)
+console.log(typeof val)
+console.log(typeof value)
+console.log(typeof values)  
